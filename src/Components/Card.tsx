135c8a19c96489ba6bb8b5bf9109448c8e44beef
jonmejia <jonathan@dataminded.net>
2023-11-13 18:38:08 -0500

    feat: updated card component so it now accepts 2 image props, and changes on hover

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
-
+import { useState } from 'react';
 interface CardImageProps {
     altText: string,
-    srcImg: string,
+    displayImage: string,
+    hoverImage: string
     itemName: string,
     color: string,
     price: number,
 }
 
-const Card: React.FC<CardImageProps> = ({altText,srcImg, itemName, color, price}):JSX.Element => {
+const Card: React.FC<CardImageProps> = ({altText,displayImage,hoverImage, itemName, color, price}):JSX.Element => {
+    const [srcImg, setSrcImg] = useState(displayImage)
      return(
          <div>
             <Image
+            onMouseEnter={()=>setSrcImg(hoverImage)}
+            onMouseLeave={()=>setSrcImg(displayImage)}
             src={srcImg}
             height={25}
             width={25}
