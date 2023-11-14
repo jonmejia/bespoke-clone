283c2655d1004c657339e96c807f2ee0d1eb88aa
jonmejia <jonathan@dataminded.net>
2023-11-13 19:16:14 -0500

    feat: imported header into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Header from '@/Components/Header'
 export default function Home() {
   return (
 <div>
-  bespoke clone
+  <Header/>
 </div> )
 }
